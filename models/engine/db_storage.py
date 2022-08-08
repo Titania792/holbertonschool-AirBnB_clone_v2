f0b112fa82c33e7ef7665137da048d8c535a36cd
Nachop51 <nachoperalta0@gmail.com>
2022-08-08 20:06:41 -0300

    final commit of the day

---
+++ models/engine/db_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""  """
+
+
+class DBStorage():
+    """ """
+    __engine = None
+    __sessions = None
+
+    def __init__(self):
+        """ """
+        self.__engine 
--- models/state.py
+++ models/state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+import models
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    @property
+    def cities(self):
+        """ Getter method """
+        from city import City
+        return [obj for obj in models.storage.all(City).values() if
+                obj.state_id == self.id]
